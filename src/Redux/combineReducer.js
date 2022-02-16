32bf16356fa4d951f2bfc707845bfd54d0219649
SukhrobWeb <83048281+astonaqulovsukhrob@users.noreply.github.com>
2022-02-16 18:05:52 +0300

    Update combineReducer.js

--- src/Redux/combineReducer.js
+++ src/Redux/combineReducer.js
@@ -3,10 +3,10 @@ import { reducers } from "./Reducers";
 import { addCategore } from "./Reducers";
 import { tabsItem } from "./Reducers";
 
-const rooReducer = combineReducers({
+const rootReducer = combineReducers({
   data: reducers,
   Item: addCategore,
   tabs: tabsItem,
 });
 
-export default rooReducer;
+export default rootReducer;
